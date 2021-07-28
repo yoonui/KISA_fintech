67346ffc61ce15f01810545c74e775a2cc6157e6
yoonui <yoony0612@ajou.ac.kr>
2021-07-28 14:11:48 +0900

    KISA fintech_react - App.js edit

--- fintech_react/src/App.js
+++ fintech_react/src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import Header from "./components/Header"
+
+const Element = ({username}) => {
+  return (
+    <>
+      <h1>안녕하세요. {username}의 새로운 컴포넌트 입니다.</h1>
+    </>
+  );
+}
 
 function App() {
+  const [userInput, setuserInput] = useState("초기값");
+
+  const inputChange = (e) => {
+    const {value} = e.target;
+    console.log(value);
+    setuserInput(value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://naver.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          ㄴㅔㅇㅣㅂㅓㄹㅗ ㅇㅣㄷㅗㅇㅎㅏㅂㄴㅣㄷㅏ.
-        </a>
-      </header>
+    <div>
+      <Header title="테스트 페이지"></Header>
+      <Element username="코딩맘"></Element>
+      <b>사용자 입력 값 : {userInput}</b>
+      <br/>
+      <input onChange={inputChange}></input>
     </div>
   );
 }
